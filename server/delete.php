44fc0f18fc16ce22a446c8ed7ad35684c7e7c397
Marcin Ławniczak <marcin@lawniczak.me>
2016-11-28 18:29:57 +0100

    Update backend

--- server/delete.php
+++ server/delete.php
@@ -12,7 +12,6 @@ include 'common.php';
 $dbh = new PDO("sqlite:$dbpath");
 //Insert element
 
-$stmt = $dbh->prepare("DELETE FROM data WHERE id = :id LIMIT 1;");
+$stmt = $dbh->prepare('DELETE FROM data WHERE id = :id;');
 $stmt->bindParam(':id', $_POST['id']);
-
 $stmt->execute();
